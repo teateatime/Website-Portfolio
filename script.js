5892f2758e09828ce682080723696036035dc861
Tim Truong <95136892+teateatime@users.noreply.github.com>
2022-03-26 22:30:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ function darkMode() {
     element.classList.toggle("dark-mode");
 }
 
+var inDarkMode = false;
 
+function toggleIcon() {
+    if (inDarkMode) {
+        document.body.classList.remove('dark-mode');
+        document.getElementById('icon-theme').innerHTML =
+          '<i class="fa fa-sun-o" id="icon-theme" style="color:#d8c658;"></i>';
+        inDarkMode = false;
+    } else {
+        document.body.classList.add('dark-mode');
+        document.getElementById('icon-theme').innerHTML =
+        '<i class="fa fa-moon-o" id="icon-theme" style="color:#737eac;"></i>';
+        inDarkMode = true;
+    }
+}
